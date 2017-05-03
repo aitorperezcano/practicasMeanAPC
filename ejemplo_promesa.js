ff384386129098dcd70ee059d6c48b75c1620706
aitorperezcano <aitorperezcano@gmail.com>
2017-05-03 14:04:25 +0200

    Bajar timeout

--- ejemplo_promesa.js
+++ ejemplo_promesa.js
@@ -6,7 +6,7 @@ function addToArray(data, array, callback) {
         callback(null, array);
     }
 }
-var array = [1, 2, 3];
+
 /*
 addToArray(4, array, function(err) {
     if (err) return console.log(err.message);
@@ -20,15 +20,17 @@ addToArray(4, array, function(err) {
     });
 });*/
 
-function addToArrayPromise(data, array) {
+addToArrayPromise = function(data, array) {
     var promise = new Promise(
         function(resolve, reject) {
-            if (array.length > 5)
+            /*if (array.length > 5) {
                 array = null;
+            }*/
+
             setTimeout(function() {
                 array.push(data);
                 resolve(array);
-            }, 1000);
+            }, 100);
 
             if (!array) {
                 reject(new Error("No existe el array"));
@@ -37,9 +39,13 @@ function addToArrayPromise(data, array) {
     );
     return promise;
 }
+
+var array = [1, 2, 3];
 addToArrayPromise(4, array)
     .then(function() { return addToArrayPromise(5, array) })
     .then(function() { return addToArrayPromise(6, array) })
     .then(function() { return addToArrayPromise(7, array) })
     .then(function() { console.log(array) })
     .catch(err => console.log(err.message));
+
+module.exports.addToArrayPromise = addToArrayPromise;
